bfab28c1284e884354f3d81b636783045887eb24
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2023-06-21 23:04:31 -0700

    oops forgot to remove the js stuff

--- Home page/home.js
+++ Home page/home.js
@@ -11,19 +11,3 @@ function start() {
     welcome.innerHTML = math.innerHTML;
     startButton.style.display = "none";
 }
-
-//select button
-var select = document.getElementById("select");
-
-var six = document.getElementById("six");
-var seven = document.getElementById("seven");
-var eight= document.getElementById("eight");
-var alg1 = document.getElementById("alg1");
-var geo = document.getElementById("geo");
-var alg2 = document.getElementById("alg2");
-var precalc = document.getElementById("precalc");
-var calcAB = document.getElementById("calcAB");
-var calcBC = document.getElementById("calcBC");
-var stats = document.getElementById("stats");
-var unitCircle = document.getElementById("unitCircle");
-var unitConversion = document.getElementById("unitConversion");
