1fb0d62b84ac089e47604da292cb1d8ee7685718
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2023-06-21 23:01:13 -0700

    button + pages are done

--- Home page/home.js
+++ Home page/home.js
@@ -5,19 +5,25 @@ var startButton = document.getElementById("start");
 var math = document.getElementById("math");
 var welcome = document.getElementById("welcome");
 
-//select button
-/*
-var selectButton = document.getElementById("select");
-
-selectButton.addEventListener("click", newPage);
-function newPage() {
-    
-}
-*/
-
 //allows start! to work
 startButton.addEventListener("click", start);
 function start() {
     welcome.innerHTML = math.innerHTML;
     startButton.style.display = "none";
 }
+
+//select button
+var select = document.getElementById("select");
+
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight= document.getElementById("eight");
+var alg1 = document.getElementById("alg1");
+var geo = document.getElementById("geo");
+var alg2 = document.getElementById("alg2");
+var precalc = document.getElementById("precalc");
+var calcAB = document.getElementById("calcAB");
+var calcBC = document.getElementById("calcBC");
+var stats = document.getElementById("stats");
+var unitCircle = document.getElementById("unitCircle");
+var unitConversion = document.getElementById("unitConversion");
